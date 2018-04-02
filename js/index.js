d9de15326877f2b3b66ef18e8ea8de3ffe7eb65f
itagn <itagn@foxmail.com>
2018-04-02 20:10:45 +0800

    新增标签和符号闭合功能

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const code = document.getElementById("code");
+CodeMirror.commands.autocomplete = function(cm) {
+    cm.showHint({hint: CodeMirror.hint.anyword});
+};
 let editor = CodeMirror.fromTextArea(code, {
     // 是否智能缩进
     smartIndent : true,
@@ -8,10 +11,12 @@ let editor = CodeMirror.fromTextArea(code, {
     mode: config.mode,
     // 设置主题
     theme: config.theme || 'seti',
+    // 隐藏滚动条
+    scrollbarStyle: 'null',
     // 缩进单位，默认2
     indentUnit: 4,
-	// 隐藏滚动条
-    scrollbarStyle: 'null',
+    // n tab = n tab
+    // indentWithTabs: true,
     // 自动获取焦点
     autofocus: true,
     //代码折叠
@@ -21,19 +26,19 @@ let editor = CodeMirror.fromTextArea(code, {
     //CodeMirror-lint-markers是实现语法报错功能
     lint: true,
     // 全屏模式
-    fullScreen:true,
+    fullScreen: true,
     // 括号匹配
-    matchBrackets:true,
-    //快捷键
+    matchBrackets: true,
+    // 自动闭合标签
+    autoCloseTags: true,
+    autoCloseBrackets: true,
+    // 快捷键
     extraKeys:{
-        "Ctrl-Enter": "autocomplete", //ctrl-enter唤起智能提示
+        // "Ctrl-Enter": "autocomplete", //ctrl-enter唤起智能提示
         "Ctrl-/": "toggleComment",
         "Ctrl-S": function (){
             let code = editor.getValue();
             alert(code);
-        },
-        "Shift-Tab": function(){
-            editor.redo();
         },
         "Ctrl-Z":function (editor) {
             editor.undo();
@@ -43,6 +48,25 @@ let editor = CodeMirror.fromTextArea(code, {
         }
     }
 });
-CodeMirror.commands.autocomplete = function(cm) {
-    cm.showHint({hint: CodeMirror.hint.anyword});
-};
+
+editor.on('change', function(instance, changObj){
+    if(changObj.text[0].trim() !== '' && changObj.text[0] !== '\n'){}
+});
+//  enter键检查
+editor.on('beforeChange', function(instance, changObj, e){
+    e = e || window.event;
+    if(e.keyCode === 13){
+        let line = editor.getCursor();
+        let str = editor.getRange({
+            line: line.line,
+            ch: line.ch-1
+        }, {
+            line: line.line,
+            ch: line.ch
+        })
+        //  最后一个字符 不为 ; 时候进行缩进
+        if(str !== ';' && str.trim() !== ''){
+            // alert(str)
+        }
+    }
+})
